d811553d2fa544fd6b0b03baf7a24938d646d415
paul7496 <paul7496@stud.kea.dk>
2022-09-09 13:36:57 +0200

    GitExercise

---
+++ src/SuperGreeter.java
@@ -0,0 +1,13 @@
+public class SuperGreeter {
+    public void start () {
+        System.out.println("Det virker!");
+    }
+
+    public static void main(String[] args){
+    SuperGreeter sg = new SuperGreeter();
+    sg.start();
+
+    }
+
+}
+
